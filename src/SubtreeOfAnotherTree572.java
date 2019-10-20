0b8e49aa2ef280ff7336dbb1cc05f5ba4357a676
Rachel Zeng <ruiqi.zzz@gmail.com>
2019-10-20 16:17:33 -0400

    done

---
+++ src/SubtreeOfAnotherTree572.java
@@ -0,0 +1,31 @@
+public class SubtreeOfAnotherTree572 {
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null)
+            return false;
+        if (isEqual(s, t))
+            return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+
+    }
+
+    public boolean isEqual(TreeNode s, TreeNode t) {
+        if (s == null && t == null)
+            return true;
+        if (s == null || t == null)
+            return false;
+        if (s.val != t.val)
+            return false;
+        return isEqual(s.left,t.left) && isEqual(s.right, t.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
