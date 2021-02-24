06341b262e3dd720f0b9cd1fd2d17c59daeaa1f0
Rachel Zeng <ruiqi.zzz@gmail.com>
2021-02-24 14:32:00 -0500

    backtrack

---
+++ 22GenerateParentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList();
+        backtrack(list, "", 0, 0, n);
+        
+        return list;
+    }
+    
+    public void backtrack(List<String> list, String curr, int open, int close, int n) {
+        if (curr.length() == n*2) {
+            list.add(curr);
+            curr = "";
+        }
+        
+        if (open < n) {
+            backtrack(list, curr + "(", open + 1, close, n);
+        }
+        
+        if (close < open) {
+            backtrack(list, curr + ")", open, close + 1, n);
+        }
+    }
+}
